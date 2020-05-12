dea0d5d07760f86927255dce9967c946aef468fc
岸川莉緒 <brri15052@gmail.com>
2020-05-12 16:52:54 +0900

    新たにテーブルを作成した

---
+++ lamp_dock/append_schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `history` (
+  `cart_id` int(11) NOT NULL,
+  `item_id` int(11) NOT NULL,
+  `amount` int(11) NOT NULL,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
